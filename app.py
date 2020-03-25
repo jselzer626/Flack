7564bf981af3acb3596f99ed9348797f4befac88
Jon Selzer <jselzer626@gmail.com>
2020-03-25 13:33:42 -0400

    added grid layout for UI

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 socketio = SocketIO(app)
-
 
 @app.route("/")
 def index():
