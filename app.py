bd371b883cbc271c1c8f9172bc33f2581b8bc061
Jon Selzer <jselzer626@gmail.com>
2020-03-27 16:41:11 -0400

    add channel create event with server storage

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 socketio = SocketIO(app)
 
+channel_content = {}
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@socketio.on("create channel")
+def create_channel(data):
+    new_channel_name = data['newChannelName']
+    channel_content.update({new_channel_name: []})
+    print(channel_content)
+    emit("confirm channel creation", {'newChannelName': new_channel_name, 'message': f"{new_channel_name} succesfully created!"}, broadcast=True)
--- static/index.js
+++ static/index.js
@@ -7,9 +7,47 @@ let greeting = (status, userName, returnGreeting, newUserSpace) => {
   returnGreeting.appendChild(greetingText)
   greetingText.className = "lead"
 
-  status == "returnUser" ? greetingText.innerHTML = `Welcome back ${userName}!` : greetingText.innerHTML = `Welcome ${userName}` ;
-
+  status == "returnUser" ? greetingText.innerHTML = `Welcome back ${userName}!` : greetingText.innerHTML = `Welcome ${userName}`
 }
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  let channelList = ''
+
+  //check to see if channels saved in browser and update variables
+  localStorage.getItem('channelList') ? channelList = localStorage.getItem('channelList') : ''
+
+  channelList != '' ? channelList.trim().split('     ').forEach(channel => channelDisplay.innerHTML += `<li>${channel}</li>`) : channelDisplay.innerHTML = "<em>No channels yet!</em>"
+
+  //connect to socket
+  let socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+  //once connection is made configure buttons
+  socket.on('connect', () => {
+
+    document.querySelector('#channelCreate').onclick = () => {
+
+      //add something later to handle if a blank name is inputted
+      let newChannelName = document.querySelector("#newChannelName").value
+
+      channelList += (newChannelName + '     ')
+      localStorage.setItem('channelList', channelList)
+      socket.emit('create channel', {'newChannelName': newChannelName})
+
+    }
+
+  })
+
+  socket.on('confirm channel creation', data => {
+
+    document.querySelector('#channelAlertSpace').innerHTML = data.message
+    data.newChannelName ? document.querySelector("#channelDisplay").innerHTML += `<li>${data.newChannelName}</li>` : ''
+
+  })
+
+})
 
 /*let channelCreate = (channelName, channelList) => {
 
